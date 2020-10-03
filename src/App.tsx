d456179ee80e9480327f6546282d5ffba7c38c0e
asucarlos <kuwahara.mee@gmail.com>
2020-10-03 11:40:23 -0400

    add proxy to resolve CORS error and set up socket

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState, FormEvent } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client'
+const ENDPOINT = process.env.ENDPOINT || "http://localhost:8000"
+
 
 function App() {
+  const [message, setMessage] = useState<string>('')
+  const socket = io(ENDPOINT);
+  useEffect(() => {
+    socket.on('FromAPI', function(msg: string){
+      setMessage(msg)
+  })
+  }, [socket])
+
+  function handleSubmit (e: FormEvent<HTMLInputElement>) {
+    console.log(e.currentTarget)
+    e.preventDefault()
+    socket.emit('message', e.currentTarget)
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +26,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        {message}
+        <input type="text" onChange={e => console.log(e.target.value)} onSubmit={e => handleSubmit(e)} />
         <a
           className="App-link"
           href="https://reactjs.org"
